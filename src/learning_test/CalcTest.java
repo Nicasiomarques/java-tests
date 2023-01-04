a425d4c8e8bf19ef7a7c0246590b4910cad6b28c
Nicasiomarques <nicasiomarques18@gmail.com>
2023-01-04 10:39:52 +0100

    test: ensure should sum 2 negative numbers

--- src/learning_test/CalcTest.java
+++ src/learning_test/CalcTest.java
@@ -5,9 +5,24 @@ import org.junit.jupiter.api.Test;
 
 class CalcTest {
 	Calc calc = new Calc();
-	
+
 	@Test
-	void ShouldSum2PositiveNumbers() {
+	void shouldSum2PositiveNumbers() {
 		assertEquals(10, calc.sum(7, 3));
+	}
+
+	@Test
+	void shouldSum2NegativeNumbers() {
+		assertEquals(-10, calc.sum(-7, -3));
+	}
+
+	@Test
+	void shouldSum1PositiveAnd1NegativeNumber() {
+		assertEquals(0, calc.sum(-7, 7));
+	}
+
+	@Test
+	void shouldSum1PositiveAnd1NegativeNumber2() {
+		assertEquals(0, calc.sum(7, -7));
 	}
 }
