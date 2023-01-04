dd3b9f7e8363fc0bff2a1e2f31a93790ec9e4879
Nicasiomarques <nicasiomarques18@gmail.com>
2023-01-04 10:54:35 +0100

    feat: add employee class

---
+++ src/learning_test/Employee.java
@@ -0,0 +1,20 @@
+package learning_test;
+import java.math.BigDecimal;
+
+public class Employee {
+  private String name;
+  private BigDecimal salary;
+
+  public Employee(String name, BigDecimal salary) {
+    this.name = name;
+    this.salary = salary;
+  }
+
+  public String getName() {
+    return name;
+  }
+
+  public BigDecimal getSalary() {
+    return salary;
+  }
+}
