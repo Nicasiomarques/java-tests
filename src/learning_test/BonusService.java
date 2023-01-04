3898bf4605f9d70ecae329032616cbecd7e9a748
Nicasiomarques <nicasiomarques18@gmail.com>
2023-01-04 13:29:23 +0100

    chore: add 2 decimal houses as default and change condition approach

--- src/learning_test/BonusService.java
+++ src/learning_test/BonusService.java
@@ -1,11 +1,11 @@
 package learning_test;
-
 import java.math.BigDecimal;
+import java.math.RoundingMode;
 
 public class BonusService {
   public BigDecimal calculateBonus(Employee employee) {
     BigDecimal value = employee.getSalary().multiply(new BigDecimal("0.1"));
-    if (value.compareTo(new BigDecimal("1000")) > 0) value = BigDecimal.ZERO;
-    return value;
+    BigDecimal bonus = (value.compareTo(new BigDecimal("1000")) > 0) ? BigDecimal.ZERO : value;
+    return bonus.setScale(2, RoundingMode.HALF_UP);
   }
 }
