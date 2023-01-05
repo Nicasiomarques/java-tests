9a8a2177c898e3412cacd4d946773189891031fd
Nicasiomarques <nicasiomarques18@gmail.com>
2023-01-05 14:12:01 +0100

    refactor: remove duplication

--- src/learning_test/tests/SalaryAdjustmentTest.java
+++ src/learning_test/tests/SalaryAdjustmentTest.java
@@ -12,33 +12,28 @@ enum Performance {
 }
 
 public class SalaryAdjustmentTest {
+  public void testEmployeePerformance(Performance performance, String salary, String salaryExpected) {
+    // Arrange
+    SalaryAdjustment sut = new SalaryAdjustment();
+    Employee employee = new Employee("john doe", LocalDate.now(), new BigDecimal(salary));
+    // Act
+    sut.adjust(employee, performance);
+    // Assert
+    Assert.assertEquals(new BigDecimal(salaryExpected), employee.getSalary());
+  }
+
   @Test
   public void adjustmentShouldBe3PercentWhenPerformanceIsDesiring() {
-    SalaryAdjustment sut = new SalaryAdjustment();
-    Employee employee = new Employee("john doe", LocalDate.now(), new BigDecimal("1000"));
-    
-    sut.adjust(employee, Performance.DESIRING);
-    
-    Assert.assertEquals(new BigDecimal("1030.00"), employee.getSalary());
+    testEmployeePerformance(Performance.DESIRING, "1000.00", "1030.00");
   }
 
   @Test
   public void adjustmentShouldBe15PercentWhenPerformanceIsGod() {
-    SalaryAdjustment sut = new SalaryAdjustment();
-    Employee employee = new Employee("john doe", LocalDate.now(), new BigDecimal("1000"));
-    
-    sut.adjust(employee, Performance.GOOD);
-    
-    Assert.assertEquals(new BigDecimal("1150.00"), employee.getSalary());
+    testEmployeePerformance(Performance.GOOD, "1000.00", "1150.00");
   }
 
   @Test
   public void adjustmentShouldBe20PercentWhenPerformanceIsGreat() {
-    SalaryAdjustment sut = new SalaryAdjustment();
-    Employee employee = new Employee("john doe", LocalDate.now(), new BigDecimal("1000"));
-    
-    sut.adjust(employee, Performance.GREAT);
-    
-    Assert.assertEquals(new BigDecimal("1200.00"), employee.getSalary());
+    testEmployeePerformance(Performance.GREAT, "1000.00", "1200.00");
   }
 }
