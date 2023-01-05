433b97fc33b9d546b3526de3c88c3c948953f198
Nicasiomarques <nicasiomarques18@gmail.com>
2023-01-05 15:27:42 +0100

    chore: move service to correct folder

--- src/learning_test/services/SalaryAdjustmentService.java
+++ src/learning_test/services/SalaryAdjustmentService.java
@@ -1,11 +1,11 @@
-package learning_test.tests;
+package learning_test.services;
 
 import java.math.BigDecimal;
 
 import learning_test.entities.Employee;
 import learning_test.entities.Performance;
 
-public class SalaryAdjustment {
+public class SalaryAdjustmentService {
   public void adjust(Employee employee, Performance performance) {
     BigDecimal salary = employee.getSalary();
     employee.adjustSalary(salary.multiply(performance.percentageReadjust()).add(salary));
